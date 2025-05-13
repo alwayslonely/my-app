4e8a39172f15278f4f0812cea4cb5b4565aef1a5
qian119110 <qianjiang1987@live.cn>
2025-05-13 15:44:53 +0800

    Ant Design引入修改

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd'
+import React from 'react'
+
+const Home = () => (
+  <div className='App'>
+    <Button type='primary'>button</Button>
+  </div>
+)
+
+export default Home
