4a7aeb86b1e06da46d88c95b5b2d161ba9b21e84
qian119110 <qianjiang1987@live.cn>
2025-05-14 15:09:43 +0800

    动态Metadata

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import BlogCard from '../../components/blogCard'
 import { data } from '../../data'
+import type { Metadata } from 'next'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}): Promise<Metadata> {
+  const { id } = await params
+  console.log(id)
+  return {
+    title: `博客详情-${id}`,
+  }
+}
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,23 +1,10 @@
-'use client'
 import React from 'react'
-import { Avatar, List } from 'antd'
-import { data } from '../data'
-import Link from 'next/link'
+import BlogList from '../components/blogList'
+import type { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: '博客列表',
+}
 export default function Page() {
-  return (
-    <List
-      itemLayout='horizontal'
-      dataSource={data}
-      renderItem={(item, index) => (
-        <List.Item>
-          <List.Item.Meta
-            className='!items-center'
-            avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
-            title={<Link href={`/blog/${item.id}`}>{item.title}</Link>}
-          />
-        </List.Item>
-      )}
-    />
-  )
+  return <BlogList />
 }
